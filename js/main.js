251eb2a12e8bffe85f9cadffaec9c0405cb8bb43
Artur-Petriv <artur.petriv@gmail.com>
2021-11-04 23:08:08 +0200

    Update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 document.addEventListener('DOMContentLoaded', function(){
-	// Code
 
+	// Header -> Change background on scroll (temporal, TODO: Find solution - debounce?)
+	function headerScroll() {
+		const header = document.querySelector('.header')
+		this.scrollY >= 50
+			? header.classList.add('header_scroll')
+			: header.classList.remove('header_scroll')
+	}
+
+	window.addEventListener('scroll', headerScroll)
 })
