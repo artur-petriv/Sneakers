fc984b83fa94b7339edaaa4ee7d0357a96696e14
Artur-Petriv <artur.petriv@gmail.com>
2021-11-06 23:16:03 +0200

    Add throttle, header on scroll logic

--- js/main.js
+++ js/main.js
@@ -1,11 +1,52 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Header -> On scroll -> Change background (temporal, TODO: Find solution - debounce?)
+  // Throttle
+  function throttle(fn, ms) {
+    let isThrottled = false;
+    let prevArgs;
+    let prevThis;
+
+    function container() {
+      if (isThrottled) {
+        prevArgs = arguments;
+        prevThis = this;
+        return;
+      }
+
+      fn.apply(this, arguments);
+      isThrottled = true;
+
+      setTimeout(function () {
+        isThrottled = false;
+        if (prevArgs) {
+          container.apply(prevThis, prevArgs);
+          prevArgs = prevThis = null;
+        }
+      }, ms);
+    }
+
+    return container;
+  }
+
+  // Header -> On scroll
+  let prevY;
+
   function headerScroll() {
     const header = document.querySelector(".header");
-    this.scrollY >= 50
-      ? header.classList.add("header_scroll")
-      : header.classList.remove("header_scroll");
+
+    if (this.scrollY >= 50) {
+      header.classList.add("header_scroll");
+
+      this.scrollY > prevY
+        ? header.classList.add("header_hide")
+        : header.classList.remove("header_hide");
+    } else {
+      header.classList.remove("header_scroll");
+    }
+
+    prevY = this.scrollY;
   }
+
+  headerScroll = throttle(headerScroll, 250);
 
   window.addEventListener("scroll", headerScroll);
 
